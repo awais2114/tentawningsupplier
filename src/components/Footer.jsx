77a83af5f6b808f51d049c89eae5efe0555a2cfd
Awais-ul-Hassan <awais122004@gmail.com>
2026-06-14 17:57:10 +0500

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = ({setActiveTab}) => {
       
              <div className="mt-10">
                   <VerticalLine />
+                  
              </div>
        
         <div className="flex mt-3  mb-5 justify-center">
